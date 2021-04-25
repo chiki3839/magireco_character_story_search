3f902bb741ae199504faba141c7a8b42f88a914c
tomo109 <72334380+tomo109@users.noreply.github.com>
2021-04-25 21:42:13 +0900

    first commit

---
+++ magreco_search_GAS/filter版_魔法少女検索のみシンプル高速.js
@@ -0,0 +1,196 @@
+//Ajaxの送受信を行う。HTTP GETをハンドリングする
+//function doGet() {
+function doGet2(e) {
+  //Googleドライブ内のJSONデータのID
+  var JSONfileID = "1XFEVa2OPHLvPsTdayyGNcoMI_ejPpnr_";
+
+   
+  //入力された各種パラメータを取得する。
+  var      and_or = e.parameter.and_or;
+  var    storyAry = e.parameter.story_csv.split(',');
+  //var    netabare = e.parameter.netabare;
+  var    charaAry = e.parameter.chara_csv.split(',');
+  var        star = e.parameter.star;
+  var keywordtext = e.parameter.keywordtext;
+
+
+/*
+  var and_or = "AND";
+  var storyAry = "アニメ".split(',');
+//  var netabare = "false";
+  var charaAry = "十咎ももこ,八雲みたま".split(',');
+  var star     = "false";
+  var keywordtext = "";
+*/
+
+  if(!keywordtext){ keywordtext = "";} //keywordtextが異常値の場合、念の為空文字に変更。
+  keywordtext = keywordtext.replace(/　/g, ' ');  //全角スペースを半角に置換。
+  keywordtext = keywordtext.replace(/ +/g, ' '); //連続半角スペースを1文字に置換
+  var wordtextAry = keywordtext.split(' '); //検索ワードの配列
+  
+  //パラメータに沿って出力データを作る。
+  var addGirl = 0; //★チェックで追加された魔法少女の数
+  
+  //★印チェックされたときの処理。ver違いが実装された時に変更する。//////////////////////////////////////
+  if(star === "true"){
+    if(charaAry.indexOf("環いろは") != -1){
+      charaAry.push("環いろは(水着ver)");
+      charaAry.push("いろはちゃん");
+      charaAry.push("いろは・やちよ(決戦ver)");
+      addGirl += 3;
+    }
+    if(charaAry.indexOf("七海やちよ") != -1){
+      charaAry.push("やちよ・みふゆ(始まりver)");
+      charaAry.push("いろは・やちよ(決戦ver)");
+      addGirl +=2;
+    }
+    if(charaAry.indexOf("由比鶴乃") != -1){
+      charaAry.push("ウワサの鶴乃");
+      charaAry.push("鶴乃・フェリシア(宅配ver)");
+      addGirl +=2;
+    }
+    if(charaAry.indexOf("深月フェリシア") != -1){
+      charaAry.push("フェリシアちゃん");
+      charaAry.push("鶴乃・フェリシア(宅配ver)");
+      addGirl +=2;
+    }
+    if(charaAry.indexOf("梓みふゆ") != -1){
+      charaAry.push("やちよ・みふゆ(始まりver)");
+      addGirl +=1;
+    }
+    if(charaAry.indexOf("アリナ・グレイ") != -1){
+      charaAry.push("ホーリーアリナ");
+      addGirl +=1;
+    }
+    if(charaAry.indexOf("水波レナ") != -1){
+      charaAry.push("レナちゃん(アイドルver)");
+      charaAry.push("レナ・かえで(水着ver)");
+      addGirl +=2;
+    }
+    if(charaAry.indexOf("秋野かえで") != -1){
+      charaAry.push("レナ・かえで(水着ver)");
+      addGirl +=1;
+    }
+    if(charaAry.indexOf("十咎ももこ") != -1){
+      charaAry.push("十咎ももこ(シスターver)");
+      addGirl +=1;
+    }
+    if(charaAry.indexOf("和泉十七夜") != -1){
+      charaAry.push("和泉十七夜(ヴァンパイアver)");
+      addGirl +=1;
+    }
+    if(charaAry.indexOf("八雲みたま") != -1){
+      charaAry.push("八雲みたま(晴着ver)");
+      addGirl +=1;
+    }
+    if(charaAry.indexOf("鹿目まどか") != -1){
+      charaAry.push("鹿目まどか(晴着ver)");
+      charaAry.push("アルティメットまどか");
+      charaAry.push("まどか先輩");
+      charaAry.push("究極まどか先輩");
+      addGirl +=4;
+    }
+    if(charaAry.indexOf("暁美ほむら") != -1){
+      charaAry.push("暁美ほむら(眼鏡ver)");
+      charaAry.push("暁美ほむら(水着ver)");
+      addGirl +=2;
+    }
+    if(charaAry.indexOf("美樹さやか") != -1){
+      charaAry.push("美樹さやか(晴着ver)");
+      addGirl +=1;
+    }
+    if(charaAry.indexOf("巴マミ") != -1){
+      charaAry.push("ホーリーマミ");
+      charaAry.push("巴マミ(水着ver)");
+      addGirl +=2;
+    }
+    if(charaAry.indexOf("佐倉杏子") != -1){
+      charaAry.push("佐倉杏子(水着ver)");
+      addGirl +=1;
+    }
+    if(charaAry.indexOf("百江なぎさ") != -1){
+      charaAry.push("百江なぎさ(バレンタインver)");
+      addGirl +=1;
+    }
+    if(charaAry.indexOf("天音月夜") != -1){
+      charaAry.push("天音姉妹(水着ver)");
+      addGirl +=1;
+    }
+    if(charaAry.indexOf("天音月咲") != -1){
+      charaAry.push("天音姉妹(水着ver)");
+      addGirl +=1;
+    }
+    if(charaAry.indexOf("五十鈴れん") != -1){
+      charaAry.push("梨花・れん(クリスマスver)");
+      addGirl +=1;
+    }
+    if(charaAry.indexOf("綾野梨花") != -1){
+      charaAry.push("梨花・れん(クリスマスver)");
+      addGirl +=1;
+    }
+    if(charaAry.indexOf("万年桜のウワサ") != -1){
+      charaAry.push("万年桜のウワサ(水着ver)");
+      addGirl +=1;
+    }
+    if(charaAry.indexOf("タルト") != -1){
+      charaAry.push("タルト(ver.Final)");
+      addGirl +=1;
+    }
+    if(charaAry.indexOf("里見灯花") != -1){
+    charaAry.push("灯花・ねむ(聖夜ver)");
+    addGirl +=1;
+    }
+    if(charaAry.indexOf("柊ねむ") != -1){
+    charaAry.push("灯花・ねむ(聖夜ver)");
+    addGirl +=1;
+    }
+    if(charaAry.indexOf("二葉さな") != -1){
+    charaAry.push("ウワサのさな");
+    addGirl +=1;
+    }
+  }
+  //★キャラクタここまで////////////////////////////////
+
+  //全ヒット数をカウントする
+  var allCnt = 0;
+  //出力用データ
+  var outData = {};
+
+  //Googleドライブにおいているデータファイルをオブジェクトに変換
+  var dataObj = JSON.parse(DriveApp.getFileById(JSONfileID).getBlob().getDataAsString());
+
+  //セットしたストーリー名ごとにサーチ
+  storyAry.forEach(storyName =>{
+    //ストーリーの一行ごとにサーチ
+    outData[storyName] = dataObj[storyName].filter(storyLine =>{
+      //ストーリーの魔法少女と検索対象の魔法少女がマッチした数をカウント
+      var _ = Underscore.load();
+      var storyCharaCnt = _.intersection(storyLine[1].split('、'), charaAry).length;
+
+      //魔法少女選択の条件にhitするかチェック
+      var charamuchflg = false;
+      if(storyCharaCnt === 0){charamuchflg = false;}
+      else if(and_or === "AND" && charaAry.length - addGirl === storyCharaCnt){charamuchflg = true;}
+      else if(and_or === "OR" && storyCharaCnt > 0){charamuchflg = true;}
+      else if(and_or === "EXCLUSIVE" && storyCharaCnt === 1){charamuchflg = true;}
+      else if(and_or === "ONLY" && ((storyLine[1].match(/、/g)||[]).length + 1)
+      === storyCharaCnt && charaAry.length - addGirl === storyCharaCnt){charamuchflg = true;}
+      return charamuchflg;
+    });
+  });
+  
+     console.log(JSON.stringify(outData));
+
+  //データ作成完了。Ajaxの返信準備
+  //返信するデータテキスト
+  var responseText;
+  var out = ContentService.createTextOutput();
+  
+  //GASはクロスドメイン制限解除ずみなのでコールバック不要。直接JSONを返信。
+  responseText = JSON.stringify(outData);
+  //Mime Typeをapplication/jsonに設定
+  out.setMimeType(ContentService.MimeType.JSON);
+  //JSONテキストをセットする
+  out.setContent(responseText);
+  return out;
+}
