e1b41a2a2dae554eed2d561c2ab81f0d0bbedcdb
chiki <74088330+chiki3839@users.noreply.github.com>
2021-06-26 23:06:19 +0900

    Delete magireco_search_html/magireco_search_html directory

--- magireco_search_html/magireco_search_html/myfile/mycommon.js
+++
@@ -1,7 +0,0 @@
-//全htmlで共通処理をまとめる。
-    //呼び出したhtmlの、girlbox(name="chara")の順に 魔法少女名(value)を取り出して配列にまとめる。
-    const allnames = [];
-    const checkboxes = document.getElementsByName("chara");
-    Array.prototype.forEach.call(checkboxes, function (item) {
-        allnames.push(item.value);
-    });
