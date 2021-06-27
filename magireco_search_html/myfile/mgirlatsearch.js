684514788c354fedb9f5cf534b4a124867b3a496
chiki <chiki3839@gmail.com>
2021-06-28 08:53:11 +0900

    呼びかけデータをMap/Setオブジェクトに変更。 Readmeの修正

--- magireco_search_html/myfile/mgirlatsearch.js
+++ magireco_search_html/myfile/mgirlatsearch.js
@@ -1,4 +1,4 @@
-function magicalGirlAttributeSearch() {
+function magicalGirlAttributeSearch(htmlName) {
     //属性でチェックされた値だけを配列に入れる。配列attributeに選択した値が入る。
     const elFavorites = document.querySelectorAll(
         '#at_form [name="at_attribute"]'
@@ -7,31 +7,33 @@ function magicalGirlAttributeSearch() {
         .filter((el) => el.checked)
         .map((el) => el.value);
 
-    //もし属性チェックが設定が空の場合は、魔法少女選択と属性表示をリセットして終わり。
-    if (attribute.length === 0) {
-        document.getElementById("at_result").style.border = "";
-        document.getElementById("at_result").innerHTML = "";
-        allshow();
-        return;
-    }
-
     //絞り込み検索テキストをリセットする。
     document.getElementById('ndownword1').value = '';
     document.getElementById('ndownword2').value = '';
-    //一旦魔法少女ボックスをすべて隠す。
-    allhidden();
 
-    var charaindex = [];
+    //もし属性チェックが設定が空の場合は、魔法少女選択と属性表示をリセットして終わり。
+    if (attribute.length === 0) {
+        allShow();
+        //絞り込みリセットボタン強調を解除する。
+        ndownResetButternReset();
+
+        if(!htmlName){
+        document.getElementById("at_result").style.border = "";
+        document.getElementById("at_result").innerHTML = "";
+        }
+        return;
+    }
+
+    //一旦魔法少女ボックスをすべて隠す。
+    allHidden();
+
+    let charaindex = [];
+
     //キャラ全員の属性値オブジェクトの配列を回す。
-    charaAttribute.forEach(function (charaObj) {
-        var muchCnt = 0;
-        attribute.forEach(function (set_at_data) {
-            var cnfflag = charaObj["attribute"].some(function (
-                chara_attribute
-            ) {
-                return chara_attribute === set_at_data;
-            });
-            if (cnfflag) {
+    for (let [girlname, at] of charaAttribute) {
+        let muchCnt = 0;
+        attribute.forEach(set_at_data => {
+            if (at.has(set_at_data)) {
                 muchCnt++;
             }
         });
@@ -47,18 +49,18 @@ function magicalGirlAttributeSearch() {
             //属性検索がandで、設定値とマッチした個数が一致したとき、または属性絵検索がorでマッチした個数が1個以上のときhit。
 
             //キャラを表示する。
-            document.getElementById(charaObj["name"]).parentNode.style.display = "inline";
+            document.getElementById(girlname).parentNode.style.display = "inline";
             //allnamesをキャラ名でサーチしてindexを積む。
-            charaindex.push(allnames.findIndex(charaname => charaname === charaObj["name"]));
+            charaindex.push(allnames.findIndex(charaname => charaname === girlname));
         }
-    });
+    };
     //charaindexを数字の若い順にソートする。
     charaindex.sort((a, b) => a - b);
     //console.log(attribute);
 
     //属性検索結果を表示するhtmlを作る。
-    var at_resultHtml = "";
-    var at_and_or = document.getElementById("at_form").at_and_or.value;
+    let at_resultHtml = "";
+    let at_and_or = document.getElementById("at_form").at_and_or.value;
     at_and_or = at_and_or.toLowerCase();
 
     //属性検索結果のヘッダを作る。
@@ -66,7 +68,7 @@ function magicalGirlAttributeSearch() {
     attribute.forEach(value => {
         at_resultHtml += value + " " + at_and_or + " ";
     });
-    var delnum = -(at_and_or.length + 2);
+    const delnum = -(at_and_or.length + 2);
     at_resultHtml = at_resultHtml.slice(0, delnum);
     at_resultHtml += "</h2>";
 
@@ -76,16 +78,24 @@ function magicalGirlAttributeSearch() {
             allnames[cindex] +
             '.webp" alt="' + allnames[cindex] + '"><span class="girlName">' + allnames[cindex] + "</span></span>";
     });
+    if(!htmlName){
+    //index.htmlの場合、絞り込み結果を表示＆ボーダーのスタイルを追加
     document.getElementById("at_result").innerHTML = at_resultHtml;
-
-    //結果にボーダーのスタイルを追加
     document.getElementById("at_result").style.border = "solid 1px #ff3b66";
 }
 
+    //絞り込みリセットボタンを強調する。
+    ndownResetButternCaution();
+}
+
 //属性検索フォームのリセット設定
-function mgirlShReset() {
-    allshow();
+function mgirlShReset(htmlName) {
+    allShow();
     document.forms["at_form"].reset();
+    ndownResetButternReset();
+
+    if(!htmlName){
     document.getElementById("at_result").style.border = "";
     document.getElementById("at_result").innerHTML = "";
+    }
 }
--- magireco_search_html/myfile/mgirlnarrow.js
+++ magireco_search_html/myfile/mgirlnarrow.js
@@ -1,10 +1,10 @@
-function mgirlnarrow(html_name, id_name) {
+function mgirlNarrow(htmlName, id_name) {
     //入力データを整理する。
-    var keywordtext = document.getElementById(id_name).value.replace(/　/g, ' '); //検索ワード。全角スペースを半角スペースに置換
+    let keywordtext = document.getElementById(id_name).value.replace(/　/g, ' '); //検索ワード。全角スペースを半角スペースに置換
     keywordtext = keywordtext.replace(/ +/g, ' '); //連続半角スペースを1文字に置換
     keywordtext = keywordtext.replace(/ $/, ''); //最後の半角スペースを削除
 
-    if (html_name === "index") {
+    if (htmlName === "index") {
         //属性検索をリセットする。
         document.forms["at_form"].reset();
         document.getElementById("at_result").style.border = "";
@@ -12,40 +12,44 @@ function mgirlnarrow(html_name, id_name) {
     }
     //もし入力が空白文字かブランクだったら全box表示
     if (keywordtext === "") {
-        allshow();
+        allShow();
+        //絞り込みボタン強調を削除
+        ndownResetButternReset();
     } else {
         //一旦全魔法少女boxを消す。
-        allhidden();
+        allHidden();
 
         //入力データを半角スペースで区切って配列を作る。
-        var shownames = keywordtext.split(" ");
+        const shownames = keywordtext.split(" ");
         allnames.forEach(girlname => {
-            var kananame = document.getElementById(girlname).parentNode.dataset.kana;
+            let kananame = document.getElementById(girlname).parentNode.dataset.kana;
             if (!kananame) {
                 kananame = "";
             } //ふりがなが未設定だったときのレスキュー
             kananame = kananame.replace(/ /g, ''); //ふりがなの姓名をくっつける
 
-            var flg = shownames.some(nameword => {
+            const flg = shownames.some(nameword => {
                 return ((girlname.indexOf(nameword) > -1) || (kananame.indexOf(nameword) > -1))
             });
             if (flg) {
                 document.getElementById(girlname).parentNode.style.display = "inline";
             }
         });
+        //絞り込みリセットボタンを強調する
+        ndownResetButternCaution();
     }
-    document.getElementById("ndownword").scrollIntoView(true);
+    // document.getElementById("ndownword").scrollIntoView(true);
 }
 
 //絞り込みした全ての魔法少女ボックスを全て表示する。
-function allshow() {
+function allShow() {
     allnames.forEach(girlname => {
         document.getElementById(girlname).parentNode.style.display = "inline";
     });
 }
 
 //全ての魔法少女ボックスを全て隠す。
-function allhidden() {
+function allHidden() {
     allnames.forEach(girlname => {
         document.getElementById(girlname).parentNode.style.display = "none";
     });
